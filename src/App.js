6f6ff1e22d810e2753672122c1a94a57300fc7c5
Michael Anokye <michaelanokyej@yahoo.com>
2020-05-06 10:26:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Axios from "axios";
+import { saveAs } from "file-saver";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "",
+    receiptId: 0,
+    price1: 0,
+    price2: 0,
+  };
+
+  handleChange = ({ target: { value, name } }) =>
+    this.setState({ [name]: value });
+
+  createAndDownloadPdf = () => {
+    Axios.post("https://pdf-generator-michael-anokye.herokuapp.com/create-pdf", this.state)
+      .then(() => Axios.get("https://pdf-generator-michael-anokye.herokuapp.com/fetch-pdf", { responseType: "blob" }))
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+
+        // saveAs takes the pdf blob and the file name as params
+        saveAs(pdfBlob, "newPdf.pdf");
+      });
+  };
+
+
+
+  render() {
+    return (
+      <div className="container">
+        <div>
+          <h1>PDF GENERATOR</h1>
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Receipt ID"
+            name="receiptId"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Price 1"
+            name="price1"
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <input
+            type="number"
+            placeholder="Price 2"
+            name="price2"
+            onChange={this.handleChange}
+          />
+        </div>
+        <button onClick={this.createAndDownloadPdf}>Download PDF</button>
+      </div>
+    );
+  }
 }
 
 export default App;
